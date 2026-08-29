8011fee3a86d3ab9b55075b678b762905e1d2bf6
baeseungil171 <baeseungil171@gmail.com>
2026-08-29 12:56:17 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,128 @@
+const albumsData = {
+    album1: {
+        title: "1집 - Heavy Bass (2004.05.31)",
+        desc: "한국 힙합 역사상 가장 찬란한 명반 중 하나로 꼽히는 피타입의 정규 1집 데뷔 앨범입니다.",
+        cover: "img/album1.png",
+        defaultVideo: "dQw4w9WgXcQ",
+        tracks: [
+            ["01", "서시", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "새로운 시작 앞에서 난 가난한 가슴을 열어 말한다...\n(서시 가사)"],
+            ["02", "Skit - 보여주고 증명하라", "P-TYPE", "P-TYPE", "P-TYPE", "dQw4w9WgXcQ", "보여주고 증명하라 가사..."],
+            ["03", "돈키호테 (Feat. 휘성)", "P-TYPE, 휘성", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "돈키호테 가사..."],
+            ["04", "Skit - 힙합다운 힙합", "P-TYPE", "P-TYPE", "P-TYPE", "dQw4w9WgXcQ", "Skit 가사..."],
+            ["05", "Heavy Bass", "P-TYPE", "P-TYPE", "P-TYPE", "dQw4w9WgXcQ", "Heavy Bass 가사..."],
+            ["06", "P-Type The Big Cat", "P-TYPE", "SAATAN", "SAATAN", "dQw4w9WgXcQ", "가사..."],
+            ["07", "힙합다운 힙합", "P-TYPE", "P-TYPE", "P-TYPE", "dQw4w9WgXcQ", "가사..."],
+            ["08", "독종", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
+            ["09", "So U Wanna Be Hardcore (Feat. MC Meta)", "P-TYPE, MC Meta", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
+            ["10", "언더그라운드", "P-TYPE", "P-TYPE", "P-TYPE", "dQw4w9WgXcQ", "가사..."],
+            ["11", "Musiq Noir (Feat. C-LUV)", "P-TYPE, C-LUV", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
+            ["12", "Skit - 언어의 연주가", "P-TYPE", "Keeproots", "P-TYPE", "dQw4w9WgXcQ", "가사..."],
+            ["13", "Wild Style (Feat. 빅마마)", "P-TYPE", "P-TYPE", "P-TYPE", "dQw4w9WgXcQ", "가사..."],
+            ["14", "언어의 연주가 (Bonus Track)", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."]
+        ]
+    },
+    album2: {
+        title: "2집 - The Vintage (2008.11.27)",
+        desc: "특유의 클래식하고 묵직한 서정성이 돋보이는 정규 2집 앨범입니다.",
+        cover: "img/album2.png",
+        defaultVideo: "dQw4w9WgXcQ",
+        tracks: [
+            ["01", "Intro", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
+            ["02", "비를 위한 발라드 (Feat. Gina)", "P-Type", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
+            ["03", "Happy People (Feat. Soulman)", "P-Type", "Artisan Beats", "Artisan Beats", "dQw4w9WgXcQ", "가사..."],
+            ["04", "Music City (Feat. Ali)", "P-Type", "Artisan Beats", "Artisan Beats", "dQw4w9WgXcQ", "가사..."],
+            ["05", "A Long Time Ago (Feat. Heritage)", "P-Type", "Mild Beats", "Mild Beats", "dQw4w9WgXcQ", "가사..."],
+            ["06", "Interlude", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
+            ["07", "수컷 (Feat. 박광수)", "P-Type", "P-Type", "P-Type", "dQw4w9WgXcQ", "가사..."],
+            ["08", "젊은 날의 초상", "P-Type, Koonta, 이인관", "P-Type", "P-Type", "dQw4w9WgXcQ", "가사..."],
+            ["09", "소나기 (Feat. Soulman)", "P-Type", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
+            ["10", "후유증 (Feat. 정인, 이인관)", "P-Type, 한경훈", "한경훈", "한경훈", "dQw4w9WgXcQ", "가사..."],
+            ["11", "Poetry Sayer", "P-Type", "The Quiett", "The Quiett", "dQw4w9WgXcQ", "가사..."],
+            ["12", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
+            ["13", "비를 위한 발라드 (Inst.)", "-", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "인스트루멘탈 앨범 트랙"],
+            ["14", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
+            ["15", "소나기 (Inst.)", "-", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "인스트루멘탈 트랙"],
+            ["16", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
+            ["17", "수컷 (Inst.)", "-", "P-Type", "P-Type", "dQw4w9WgXcQ", "인스트루멘탈 트랙"]
+        ]
+    },
+    album3: {
+        title: "3집 - RAP (2013.07.29)",
+        desc: "랩이라는 장르와 그 본질에 대한 깊은 고찰이 담긴 정규 3집 앨범입니다.",
+        cover: "img/album3.png",
+        defaultVideo: "dQw4w9WgXcQ",
+        tracks: [
+            ["01", "I’m Back", "P-type", "Ging, Optical Eyez XL", "Ging, Optical Eyez XL", "dQw4w9WgXcQ", "가사..."],
+            ["02", "Love, Life, Rap (Feat. GRAY)", "P-type", "GRAY, Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
+            ["03", "볼케이노 (Feat. 선우정아, 손수경)", "P-type", "Keeproots, Fascinating, Rhymer", "Keeproots, Fascinating", "dQw4w9WgXcQ", "가사..."],
+            ["04", "꿈의 해석 (Feat. Jinbo)", "P-type", "프라이머리, Jinbo", "프라이머리", "dQw4w9WgXcQ", "가사..."],
+            ["05", "Highway Trucker", "P-type", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사..."],
+            ["06", "OST: Peace (Feat. Paloalto, B-Free, Huckleberry P)", "P-type, Paloalto, B-Free, Huckleberry P", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사..."],
+            ["07", "뿔 (Feat. BUMKEY)", "P-type, BUMKEY", "Keeproots, Fascinating, BUMKEY", "Keeproots, Fascinating", "dQw4w9WgXcQ", "가사..."],
+            ["08", "불 꺼", "P-type", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사..."],
+            ["09", "OST: Respect (Feat. Beenzino, Dok2)", "P-type, Beenzino, Dok2", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사..."],
+            ["10", "OST: One Love (Feat. Noise Mob)", "P-type, Minos, RHYME-A-", "Score 13", "Score 13", "dQw4w9WgXcQ", "가사..."],
+            ["11", "Unchangeable (Feat. Sean2slow)", "P-type, Sean2slow", "Optical Eyez XL", "Optical Eyez XL", "dQw4w9WgXcQ", "가사..."],
+            ["12", "다이하드 (Feat. Ali, MC Meta)", "P-type, MC Meta", "Artisan Beats, Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."]
+        ]
+    },
+    album4: {
+        title: "4집 - Street Poetry (2015.03.20)",
+        desc: "거리의 시인으로서 피타입의 날카로운 통찰력과 가사가 돋보이는 4집 앨범입니다.",
+        cover: "img/album4.png",
+        defaultVideo: "dQw4w9WgXcQ",
+        tracks: [
+            ["01", "폭력적인 잡종문화", "P-Type", "Ronald Bell, R. Jackson, Deepfry", "Kay Gee's, Deepfry", "dQw4w9WgXcQ", "가사..."],
+            ["02", "Do The Right Rap (Feat. Huckleberry P)", "P-Type, Huckleberry P", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사..."],
+            ["03", "Timberland 6\" (Feat. NUCK of Souldive)", "P-Type, NUCK", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
+            ["04", "네안데르탈 (Feat. Minos, Justhis)", "P-Type, Minos, Justhis", "Deepfry", "Deepfry", "dQw4w9WgXcQ", "가사..."],
+            ["05", "광화문 (Feat. 태완)", "P-Type", "Fascinating, 김태완", "Fascinating, YELLA Diamond", "dQw4w9WgXcQ", "가사..."],
+            ["06", "돈키호테2 (Feat. 바버렛츠)", "P-Type", "Fascinating, 김은혜", "Fascinating, DJ Juice", "dQw4w9WgXcQ", "가사..."],
+            ["07", "이방인 (Feat. 차붐)", "Neil Young, P-Type, 차붐", "Neil Young, Deepfry", "Neil Young, Deepfry", "dQw4w9WgXcQ", "가사..."],
+            ["08", "반환점", "P-Type", "김창완, 김창호", "김창완, 김영진, 김제형", "dQw4w9WgXcQ", "가사..."],
+            ["09", "최악의 남자", "P-Type", "Fascinating", "Fascinating, DJ Juice, P-Type", "dQw4w9WgXcQ", "가사..."],
+            ["10", "Vice Versa (Feat. 선우정아)", "선우정아, P-Type", "Kebee, 박민우, 선우정아", "Kebee, 박민우", "dQw4w9WgXcQ", "가사..."],
+            ["11", "광화문 (Inst.)", "-", "Fascinating, 김태완", "Fascinating, YELLA Diamond", "dQw4w9WgXcQ", "인스트루멘탈"],
+            ["12", "돈키호테2 (Inst.)", "-", "Fascinating, 김은혜", "Fascinating, DJ Juice", "dQw4w9WgXcQ", "인스트루멘탈"],
+            ["13", "반환점 (Inst.)", "-", "김창완, 김창호", "김창완, 김영진, 김제형", "dQw4w9WgXcQ", "인스트루멘탈"]
+        ]
+    },
+    album5: {
+        title: "5집 - Hardboiled Café 5 (2022.02.18)",
+        desc: "하드보일드한 감성을 담아내며 건재함을 증명한 정규 5집 앨범입니다.",
+        cover: "img/album5.png",
+        defaultVideo: "dQw4w9WgXcQ",
+        tracks: [
+            ["01", "Writer's Profile", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
+            ["02", "Intro (Feat. WRKMS, 김현심)", "P-TYPE", "WRKMS", "WRKMS, P-TYPE", "dQw4w9WgXcQ", "가사..."]
+        ]
+    }
+};
+
+const singlesInfo = [
+    { name: "Soulfire", date: "2006.09.07.", type: "싱글 1집" },
+    { name: "다이하드", date: "2013.03.22.", type: "싱글" },
+    { name: "Lyrical Saurus", date: "2013.11.08.", type: "무료 음원" },
+    { name: "불편한 관계", date: "2013.11.27.", type: "싱글" },
+    { name: "Timberland 6\"", date: "2014.05.28.", type: "싱글" },
+    { name: "광화문", date: "2015.03.11.", type: "싱글" },
+    { name: "버드맨", date: "2015.07.06.", type: "싱글" },
+    { name: "시차적응", date: "2015.11.09.", type: "싱글" },
+    { name: "게으르게", date: "2016.07.04.", type: "싱글" },
+    { name: "2 years part.1", date: "2017.06.19.", type: "더블 싱글" },
+    { name: "Explicit Content", date: "2020.02.03.", type: "더블 싱글" },
+    { name: "블루문특급", date: "2020.05.03.", type: "싱글" },
+    { name: "바벨", date: "2020.07.03.", type: "싱글" },
+    { name: "전태일 랩소디", date: "2020.11.19.", type: "싱글" },
+    { name: "신도시", date: "2021.02.18.", type: "싱글" },
+    { name: "Ordinary Verse", date: "2021.03.18.", type: "싱글" },
+    { name: "The Dog Walker", date: "2021.07.22.", type: "싱글" },
+    { name: "MIC \"The Hammer\"", date: "2022.02.11.", type: "싱글" },
+    { name: "2022 영등포", date: "2022.10.18.", type: "싱글" }
+];
+
+const participationInfo = [
+    { name: "돈의 화신 OST - MONEY", date: "2013.03.16.", type: "참여 음반", desc: "SBS 드라마 OST" },
+    { name: "스노우볼 프로젝트 Vol.3", date: "2016.12.16.", type: "참여 음반", desc: "크라잉넛과 협업" },
+    { name: "무증상", date: "2021.06.16.", type: "참여 음반", desc: "P-TYPE & OPTICAL EYEZ XL" }
+];

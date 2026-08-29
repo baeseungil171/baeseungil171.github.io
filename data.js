e93f9e1f856bb421bf0a167df81016b5e7cd7032
baeseungil171 <baeseungil171@gmail.com>
2026-08-29 15:01:51 +0900

    Update data.js

--- data.js
+++ data.js
@@ -5,72 +5,20 @@ const albumsData = {
         cover: "img/album1.png",
         defaultVideo: "-UjhZpdCBYg",
         tracks: [
-            [
-                "01", 
-                "서시", 
-                "P-TYPE", 
-                "Keeproots", 
-                "Keeproots", 
-                "-UjhZpdCBYg", 
-                `새로운 시작 앞에서 난 가난한 가슴을 열어 말한다.
-어두운 밤 한 가운데 거친 방황과
-수 없이 만났어도 살아남았다고.
-쉽지는 않았어도 예까지 왔다고.
-
-사막 같은 도시를 사나운 바람
-벗 삼아 걷고 있는 사나이.
-세상 앞에 울부짖는 숫 사자다.
-시작 앞에서 난 혼자다.
-
-이제는 펜을 내려놓고 마이크를 잡아.
-길었던 방황 끝에서 초라한 꼴로 살아남았다.
-이제 거친 세상에 홀로 맞선다.
-난 섬광을 뚫고 저 무대로 다가선다.
-
-낯선 자들의 거친 함성과
-어우러져 노래 부른다. 마이크는 달궈졌다.
-여기 모인 구름 같은 관중들은 다 날 부른다.
-긴 잠에서 깨어 이제 눈을 뜨는가......
-
-내 힙합은 일탈의 도구가 아니다.
-여기 짓밟힌 이 땅의 빛바랜 힙합을
-힙합다운 힙합으로 밑바닥부터
-다시 채워놓기 위한 나직한 비판이다.
-
-이제 여기 내 이야기 펼쳐 놓으리라.
-붉은 저 노을이 나의 분신인양
-애처로운 이 날, 거칠 것 없는 혼자다.
-이제는 펜을 내려놓고 마이크를 잡아.
-
-대안을 잉태한 체 난 문장을 토한다.
-위대한 저 태양 아래 진실을 고한다.
-나태한 자신에 대한 짧은 회개와
-그대와 나에 대한 짤막한 대화.
-
-난 펜을 휘갈겨 세상을 태운다.
-어둠을 밝혀 아침을 깨운다.
-새벽 공기 속에 나의 언어를 채운다.
-붉은 여명 아래 나의 음성을 채운다.
-
-정상을 찾아갈 난 지금 혼자다.
-이제는 펜을 내려놓고 마이크를 잡아.
-
-정상을 찾아갈 난 지금 혼자다.
-이제는 펜을 내려놓고 마이크를 잡아.`
-            ],
-            ["02", "Skit - 보여주고 증명하라", "P-TYPE", "P-TYPE", "P-TYPE", "vwglS14KLYA", "보여주고 증명하라 가사..."],
-            ["03", "돈키호테 (Feat. 휘성)", "P-TYPE, 휘성", "Keeproots", "Keeproots", "s4DlADgREec", "돈키호테 가사..."],
-            ["04", "Skit - 힙합다운 힙합", "P-TYPE", "P-TYPE", "P-TYPE", "KVK1BKb8", "Skit 가사..."],
-            ["05", "Heavy Bass", "P-TYPE", "P-TYPE", "P-TYPE", "fhLRh2Diu8A", "Heavy Bass 가사..."],
-            ["06", "P-Type The Big Cat", "P-TYPE", "SAATAN", "SAATAN", "n48bwCPniR4", "가사..."],
-            ["07", "힙합다운 힙합", "P-TYPE", "P-TYPE", "P-TYPE", "aTFLybqeiPM", "가사..."],
-            ["08", "독종", "P-TYPE", "Keeproots", "Keeproots", "89TXO-kW4Ik", "가사..."],
-            ["09", "So U Wanna Be Hardcore (Feat. MC Meta)", "P-TYPE, MC Meta", "Keeproots", "Keeproots", "6CSG4qBUwzs", "가사..."],
-            ["10", "언더그라운드", "P-TYPE", "P-TYPE", "P-TYPE", "YV5s5uQ-UyU", "가사..."],
-            ["11", "Musiq Noir (Feat. C-LUV)", "P-TYPE, C-LUV", "Keeproots", "Keeproots", "xrX474-XD0A", "가사..."],
-            ["12", "Skit - 언어의 연주가", "P-TYPE", "Keeproots", "P-TYPE", "LUPMTJKrL-U", "가사..."],
-            ["13", "Wild Style (Feat. 빅마마)", "P-TYPE", "P-TYPE", "P-TYPE", "62jtThWYwWs", "가사..."],
-            ["14", "언어의 연주가 (Bonus Track)", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."]
+            ["01", "서시", "P-TYPE", "Keeproots", "Keeproots", "-UjhZpdCBYg", "album1_0"],
+            ["02", "Skit - 보여주고 증명하라", "P-TYPE", "P-TYPE", "P-TYPE", "vwglS14KLYA", "album1_1"],
+            ["03", "돈키호테 (Feat. 휘성)", "P-TYPE, 휘성", "Keeproots", "Keeproots", "s4DlADgREec", "가사가 없습니다."],
+            ["04", "Skit - 힙합다운 힙합", "P-TYPE", "P-TYPE", "P-TYPE", "KVK1BKb8", "가사가 없습니다."],
+            ["05", "Heavy Bass", "P-TYPE", "P-TYPE", "P-TYPE", "fhLRh2Diu8A", "가사가 없습니다."],
+            ["06", "P-Type The Big Cat", "P-TYPE", "SAATAN", "SAATAN", "n48bwCPniR4", "가사가 없습니다."],
+            ["07", "힙합다운 힙합", "P-TYPE", "P-TYPE", "P-TYPE", "aTFLybqeiPM", "가사가 없습니다."],
+            ["08", "독종", "P-TYPE", "Keeproots", "Keeproots", "89TXO-kW4Ik", "가사가 없습니다."],
+            ["09", "So U Wanna Be Hardcore (Feat. MC Meta)", "P-TYPE, MC Meta", "Keeproots", "Keeproots", "6CSG4qBUwzs", "가사가 없습니다."],
+            ["10", "언더그라운드", "P-TYPE", "P-TYPE", "P-TYPE", "YV5s5uQ-UyU", "가사가 없습니다."],
+            ["11", "Musiq Noir (Feat. C-LUV)", "P-TYPE, C-LUV", "Keeproots", "Keeproots", "xrX474-XD0A", "가사가 없습니다."],
+            ["12", "Skit - 언어의 연주가", "P-TYPE", "Keeproots", "P-TYPE", "LUPMTJKrL-U", "가사가 없습니다."],
+            ["13", "Wild Style (Feat. 빅마마)", "P-TYPE", "P-TYPE", "P-TYPE", "62jtThWYwWs", "가사가 없습니다."],
+            ["14", "언어의 연주가 (Bonus Track)", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."]
         ]
     },
     album2: {
@@ -79,22 +27,22 @@ const albumsData = {
         cover: "img/album2.png",
         defaultVideo: "dQw4w9WgXcQ",
         tracks: [
-            ["01", "Intro", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
-            ["02", "비를 위한 발라드 (Feat. Gina)", "P-Type", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
-            ["03", "Happy People (Feat. Soulman)", "P-Type", "Artisan Beats", "Artisan Beats", "dQw4w9WgXcQ", "가사..."],
-            ["04", "Music City (Feat. Ali)", "P-Type", "Artisan Beats", "Artisan Beats", "dQw4w9WgXcQ", "가사..."],
-            ["05", "A Long Time Ago (Feat. Heritage)", "P-Type", "Mild Beats", "Mild Beats", "dQw4w9WgXcQ", "가사..."],
-            ["06", "Interlude", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
-            ["07", "수컷 (Feat. 박광수)", "P-Type", "P-Type", "P-Type", "dQw4w9WgXcQ", "가사..."],
-            ["08", "젊은 날의 초상", "P-Type, Koonta, 이인관", "P-Type", "P-Type", "dQw4w9WgXcQ", "가사..."],
-            ["09", "소나기 (Feat. Soulman)", "P-Type", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
-            ["10", "후유증 (Feat. 정인, 이인관)", "P-Type, 한경훈", "한경훈", "한경훈", "dQw4w9WgXcQ", "가사..."],
-            ["11", "Poetry Sayer", "P-Type", "The Quiett", "The Quiett", "dQw4w9WgXcQ", "가사..."],
-            ["12", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
+            ["01", "Intro", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["02", "비를 위한 발라드 (Feat. Gina)", "P-Type", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["03", "Happy People (Feat. Soulman)", "P-Type", "Artisan Beats", "Artisan Beats", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["04", "Music City (Feat. Ali)", "P-Type", "Artisan Beats", "Artisan Beats", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["05", "A Long Time Ago (Feat. Heritage)", "P-Type", "Mild Beats", "Mild Beats", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["06", "Interlude", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["07", "수컷 (Feat. 박광수)", "P-Type", "P-Type", "P-Type", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["08", "젊은 날의 초상", "P-Type, Koonta, 이인관", "P-Type", "P-Type", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["09", "소나기 (Feat. Soulman)", "P-Type", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["10", "후유증 (Feat. 정인, 이인관)", "P-Type, 한경훈", "한경훈", "한경훈", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["11", "Poetry Sayer", "P-Type", "The Quiett", "The Quiett", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["12", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사가 없습니다."],
             ["13", "비를 위한 발라드 (Inst.)", "-", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "인스트루멘탈 앨범 트랙"],
-            ["14", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
+            ["14", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사가 없습니다."],
             ["15", "소나기 (Inst.)", "-", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "인스트루멘탈 트랙"],
-            ["16", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사..."],
+            ["16", "Skit", "P-Type", "-", "-", "dQw4w9WgXcQ", "가사가 없습니다."],
             ["17", "수컷 (Inst.)", "-", "P-Type", "P-Type", "dQw4w9WgXcQ", "인스트루멘탈 트랙"]
         ]
     },
@@ -104,18 +52,18 @@ const albumsData = {
         cover: "img/album3.png",
         defaultVideo: "dQw4w9WgXcQ",
         tracks: [
-            ["01", "I’m Back", "P-type", "Ging, Optical Eyez XL", "Ging, Optical Eyez XL", "dQw4w9WgXcQ", "가사..."],
-            ["02", "Love, Life, Rap (Feat. GRAY)", "P-type", "GRAY, Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
-            ["03", "볼케이노 (Feat. 선우정아, 손수경)", "P-type", "Keeproots, Fascinating, Rhymer", "Keeproots, Fascinating", "dQw4w9WgXcQ", "가사..."],
-            ["04", "꿈의 해석 (Feat. Jinbo)", "P-type", "프라이머리, Jinbo", "프라이머리", "dQw4w9WgXcQ", "가사..."],
-            ["05", "Highway Trucker", "P-type", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사..."],
-            ["06", "OST: Peace (Feat. Paloalto, B-Free, Huckleberry P)", "P-type, Paloalto, B-Free, Huckleberry P", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사..."],
-            ["07", "뿔 (Feat. BUMKEY)", "P-type, BUMKEY", "Keeproots, Fascinating, BUMKEY", "Keeproots, Fascinating", "dQw4w9WgXcQ", "가사..."],
-            ["08", "불 꺼", "P-type", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사..."],
-            ["09", "OST: Respect (Feat. Beenzino, Dok2)", "P-type, Beenzino, Dok2", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사..."],
-            ["10", "OST: One Love (Feat. Noise Mob)", "P-type, Minos, RHYME-A-", "Score 13", "Score 13", "dQw4w9WgXcQ", "가사..."],
-            ["11", "Unchangeable (Feat. Sean2slow)", "P-type, Sean2slow", "Optical Eyez XL", "Optical Eyez XL", "dQw4w9WgXcQ", "가사..."],
-            ["12", "다이하드 (Feat. Ali, MC Meta)", "P-type, MC Meta", "Artisan Beats, Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."]
+            ["01", "I’m Back", "P-type", "Ging, Optical Eyez XL", "Ging, Optical Eyez XL", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["02", "Love, Life, Rap (Feat. GRAY)", "P-type", "GRAY, Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["03", "볼케이노 (Feat. 선우정아, 손수경)", "P-type", "Keeproots, Fascinating, Rhymer", "Keeproots, Fascinating", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["04", "꿈의 해석 (Feat. Jinbo)", "P-type", "프라이머리, Jinbo", "프라이머리", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["05", "Highway Trucker", "P-type", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["06", "OST: Peace (Feat. Paloalto, B-Free, Huckleberry P)", "P-type, Paloalto, B-Free, Huckleberry P", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["07", "뿔 (Feat. BUMKEY)", "P-type, BUMKEY", "Keeproots, Fascinating, BUMKEY", "Keeproots, Fascinating", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["08", "불 꺼", "P-type", "G-Slow", "G-Slow", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["09", "OST: Respect (Feat. Beenzino, Dok2)", "P-type, Beenzino, Dok2", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["10", "OST: One Love (Feat. Noise Mob)", "P-type, Minos, RHYME-A-", "Score 13", "Score 13", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["11", "Unchangeable (Feat. Sean2slow)", "P-type, Sean2slow", "Optical Eyez XL", "Optical Eyez XL", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["12", "다이하드 (Feat. Ali, MC Meta)", "P-type, MC Meta", "Artisan Beats, Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."]
         ]
     },
     album4: {
@@ -124,16 +72,16 @@ const albumsData = {
         cover: "img/album4.png",
         defaultVideo: "dQw4w9WgXcQ",
         tracks: [
-            ["01", "폭력적인 잡종문화", "P-Type", "Ronald Bell, R. Jackson, Deepfry", "Kay Gee's, Deepfry", "dQw4w9WgXcQ", "가사..."],
-            ["02", "Do The Right Rap (Feat. Huckleberry P)", "P-Type, Huckleberry P", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사..."],
-            ["03", "Timberland 6\" (Feat. NUCK of Souldive)", "P-Type, NUCK", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
-            ["04", "네안데르탈 (Feat. Minos, Justhis)", "P-Type, Minos, Justhis", "Deepfry", "Deepfry", "dQw4w9WgXcQ", "가사..."],
-            ["05", "광화문 (Feat. 태완)", "P-Type", "Fascinating, 김태완", "Fascinating, YELLA Diamond", "dQw4w9WgXcQ", "가사..."],
-            ["06", "돈키호테2 (Feat. 바버렛츠)", "P-Type", "Fascinating, 김은혜", "Fascinating, DJ Juice", "dQw4w9WgXcQ", "가사..."],
-            ["07", "이방인 (Feat. 차붐)", "Neil Young, P-Type, 차붐", "Neil Young, Deepfry", "Neil Young, Deepfry", "dQw4w9WgXcQ", "가사..."],
-            ["08", "반환점", "P-Type", "김창완, 김창호", "김창완, 김영진, 김제형", "dQw4w9WgXcQ", "가사..."],
-            ["09", "최악의 남자", "P-Type", "Fascinating", "Fascinating, DJ Juice, P-Type", "dQw4w9WgXcQ", "가사..."],
-            ["10", "Vice Versa (Feat. 선우정아)", "선우정아, P-Type", "Kebee, 박민우, 선우정아", "Kebee, 박민우", "dQw4w9WgXcQ", "가사..."],
+            ["01", "폭력적인 잡종문화", "P-Type", "Ronald Bell, R. Jackson, Deepfry", "Kay Gee's, Deepfry", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["02", "Do The Right Rap (Feat. Huckleberry P)", "P-Type, Huckleberry P", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["03", "Timberland 6\" (Feat. NUCK of Souldive)", "P-Type, NUCK", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["04", "네안데르탈 (Feat. Minos, Justhis)", "P-Type, Minos, Justhis", "Deepfry", "Deepfry", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["05", "광화문 (Feat. 태완)", "P-Type", "Fascinating, 김태완", "Fascinating, YELLA Diamond", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["06", "돈키호테2 (Feat. 바버렛츠)", "P-Type", "Fascinating, 김은혜", "Fascinating, DJ Juice", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["07", "이방인 (Feat. 차붐)", "Neil Young, P-Type, 차붐", "Neil Young, Deepfry", "Neil Young, Deepfry", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["08", "반환점", "P-Type", "김창완, 김창호", "김창완, 김영진, 김제형", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["09", "최악의 남자", "P-Type", "Fascinating", "Fascinating, DJ Juice, P-Type", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["10", "Vice Versa (Feat. 선우정아)", "선우정아, P-Type", "Kebee, 박민우, 선우정아", "Kebee, 박민우", "dQw4w9WgXcQ", "가사가 없습니다."],
             ["11", "광화문 (Inst.)", "-", "Fascinating, 김태완", "Fascinating, YELLA Diamond", "dQw4w9WgXcQ", "인스트루멘탈"],
             ["12", "돈키호테2 (Inst.)", "-", "Fascinating, 김은혜", "Fascinating, DJ Juice", "dQw4w9WgXcQ", "인스트루멘탈"],
             ["13", "반환점 (Inst.)", "-", "김창완, 김창호", "김창완, 김영진, 김제형", "dQw4w9WgXcQ", "인스트루멘탈"]
@@ -145,26 +93,26 @@ const albumsData = {
         cover: "img/album5.png",
         defaultVideo: "dQw4w9WgXcQ",
         tracks: [
-            ["01", "Writer's Profile", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
-            ["02", "Intro (Feat. WRKMS, 김현심)", "P-TYPE", "WRKMS", "WRKMS, P-TYPE", "dQw4w9WgXcQ", "가사..."],
-            ["03", "Hardboiled Café (Feat. 정인, FROM ALL TO HUMAN)", "P-TYPE", "Keeproots, FROM ALL TO HUMAN", "Keeproots, FROM ALL TO HUMAN, P-TYPE", "dQw4w9WgXcQ", "가사..."],
-            ["04", "깨끗하고 밝은 곳", "P-TYPE", "PLAYHOUSE", "PLAYHOUSE", "dQw4w9WgXcQ", "가사..."],
-            ["05", "The Picaresque", "P-TYPE", "Fredi Casso", "Fredi Casso", "dQw4w9WgXcQ", "가사..."],
-            ["06", "안녕, 내 사랑 (Feat. 르네, WRKMS)", "P-TYPE", "WRKMS, 르네", "P-TYPE", "dQw4w9WgXcQ", "가사..."],
-            ["07", "노인의 테마: 낚시 (Feat. 최엘비)", "P-TYPE, 최엘비", "PLAYHOUSE", "PLAYHOUSE", "dQw4w9WgXcQ", "가사..."],
-            ["08", "The Simple Art of Rhyme (Feat. KWAII)", "P-TYPE, KWAII", "Pe2ny", "Pe2ny", "dQw4w9WgXcQ", "가사..."],
-            ["09", "For Sale, My Rhymes, Never Used (Feat. JUSTHIS, DON MALIK)", "P-TYPE, JUSTHIS, DON MALIK", "Fredi Casso", "Fredi Casso", "dQw4w9WgXcQ", "가사..."],
-            ["10", "MIC \"The Hammer\" (Feat. Paloalto, Chin)", "P-TYPE, Paloalto, Chin", "Deepfry", "Deepfry, Fascinating, P-TYPE", "dQw4w9WgXcQ", "가사..."],
-            ["11", "Lyricist Paradox (Feat. DON MALIK)", "P-TYPE, DON MALIK", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
-            ["12", "Heavy Bass Saga (Feat. 김오키)", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사..."],
-            ["13", "고양이와 남자", "P-TYPE", "Fredi Casso", "Fredi Casso", "dQw4w9WgXcQ", "가사..."],
-            ["14", "Moonlighting (Feat. 서사무엘) (album version)", "P-TYPE, 서사무엘", "Keeproots, 서사무엘", "Keeproots", "dQw4w9WgXcQ", "가사..."],
-            ["15", "Big Sleep (Feat. ILLINIT)", "P-TYPE, ILLINIT", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사..."],
-            ["16", "Kiss Me Deadly (Feat. 김필)", "P-TYPE, 김필", "Fascinating, 김필", "Fascinating", "dQw4w9WgXcQ", "가사..."],
-            ["17", "고양이는 아홉번 죽는다", "P-TYPE", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사..."],
-            ["18", "부정의 부정은 긍정 (Feat. 버벌진트)", "P-TYPE, Verbal Jint", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사..."],
-            ["19", "Outro (Feat. 김현심)", "P-TYPE", "Keeproots, P-TYPE", "P-TYPE", "dQw4w9WgXcQ", "가사..."],
-            ["20", "에필로그: 씬을 위한 오드 (Feat. The Quiett, 서사무엘)", "P-TYPE, The Quiett, 서사무엘", "Keeproots, 서사무엘", "Keeproots", "dQw4w9WgXcQ", "가사..."]
+            ["01", "Writer's Profile", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["02", "Intro (Feat. WRKMS, 김현심)", "P-TYPE", "WRKMS", "WRKMS, P-TYPE", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["03", "Hardboiled Café (Feat. 정인, FROM ALL TO HUMAN)", "P-TYPE", "Keeproots, FROM ALL TO HUMAN", "Keeproots, FROM ALL TO HUMAN, P-TYPE", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["04", "깨끗하고 밝은 곳", "P-TYPE", "PLAYHOUSE", "PLAYHOUSE", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["05", "The Picaresque", "P-TYPE", "Fredi Casso", "Fredi Casso", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["06", "안녕, 내 사랑 (Feat. 르네, WRKMS)", "P-TYPE", "WRKMS, 르네", "P-TYPE", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["07", "노인의 테마: 낚시 (Feat. 최엘비)", "P-TYPE, 최엘비", "PLAYHOUSE", "PLAYHOUSE", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["08", "The Simple Art of Rhyme (Feat. KWAII)", "P-TYPE, KWAII", "Pe2ny", "Pe2ny", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["09", "For Sale, My Rhymes, Never Used (Feat. JUSTHIS, DON MALIK)", "P-TYPE, JUSTHIS, DON MALIK", "Fredi Casso", "Fredi Casso", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["10", "MIC \"The Hammer\" (Feat. Paloalto, Chin)", "P-TYPE, Paloalto, Chin", "Deepfry", "Deepfry, Fascinating, P-TYPE", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["11", "Lyricist Paradox (Feat. DON MALIK)", "P-TYPE, DON MALIK", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["12", "Heavy Bass Saga (Feat. 김오키)", "P-TYPE", "Keeproots", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["13", "고양이와 남자", "P-TYPE", "Fredi Casso", "Fredi Casso", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["14", "Moonlighting (Feat. 서사무엘) (album version)", "P-TYPE, 서사무엘", "Keeproots, 서사무엘", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["15", "Big Sleep (Feat. ILLINIT)", "P-TYPE, ILLINIT", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["16", "Kiss Me Deadly (Feat. 김필)", "P-TYPE, 김필", "Fascinating, 김필", "Fascinating", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["17", "고양이는 아홉번 죽는다", "P-TYPE", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["18", "부정의 부정은 긍정 (Feat. 버벌진트)", "P-TYPE, Verbal Jint", "Fascinating", "Fascinating", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["19", "Outro (Feat. 김현심)", "P-TYPE", "Keeproots, P-TYPE", "P-TYPE", "dQw4w9WgXcQ", "가사가 없습니다."],
+            ["20", "에필로그: 씬을 위한 오드 (Feat. The Quiett, 서사무엘)", "P-TYPE, The Quiett, 서사무엘", "Keeproots, 서사무엘", "Keeproots", "dQw4w9WgXcQ", "가사가 없습니다."]
         ]
     }
 };
